56311be084b2c21988a7fb2a1ba81b65d09d2f83
Ethan Roberts <ejrobe@gmail.com>
2018-12-31 17:54:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  i = 0
+  x = Array.new
+  while i < arr.length
+    yield(arr[i])
+    x << arr[i]
+    i += 1
+  end
+  return x
 end
