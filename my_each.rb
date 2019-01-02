dc3a8c257075aac522c588cba5540b09d50eb36a
Ethan Roberts <ejrobe@gmail.com>
2019-01-02 03:37:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
 def my_each(arr)
-  i = 0
+  counter = 0
   x = Array.new
-  while i < arr.length
-    yield(arr[i])
-    x << arr[i]
-    i += 1
+  while counter < arr.length
+    yield(arr[counter])
+    x << arr[counter]
+    counter += 1
   end
   return x
 end
